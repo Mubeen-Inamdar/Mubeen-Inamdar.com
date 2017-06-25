c67f134927ca1f4710560f77a24374e8dbc6d5ca
Mubeen Inamdar <mubeen@mubeen-inamdar.com>
2017-06-25 12:47:45 +0100

    removed cv

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,7 +3,6 @@
         <div class="nav-wrapper container">
             <a id="logo-container" href="#" class="brand-logo white-text">{{ config('app.name') }}</a>
             <ul class="right hide-on-med-and-down">
-                <li><a href="{{ url('storage/Mubeen%20Inamdar%20-%20CV%20April%202017.pdf') }}" class="white-text"><i class="material-icons right">description</i>Download My CV</a></li>
                 <li><a href="{{ config('url.blog') }}" class="white-text"><i class="material-icons right">comment</i>Blog</a></li>
             </ul>
 
@@ -13,6 +12,5 @@
 </div>
 
 <ul id="nav-mobile" class="side-nav blue-grey">
-    <li><a href="{{ url('storage/Mubeen%20Inamdar%20-%20CV%April%202017.pdf') }}" class="white-text"><i class="material-icons white-text">description</i>Download My CV</a></li>
     <li><a href="{{ config('url.blog') }}"  class="white-text"><i class="material-icons white-text">comment</i>Blog</a></li>
 </ul>
